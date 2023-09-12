0dab4759d513c6c01f7e4e00fdd156e5f48a5d8c
Pea Masamba <96761006+NadifMasamba@users.noreply.github.com>
2023-09-12 20:48:49 +0700

    Minor Changes

--- bot/modules/ytdlp.py
+++ bot/modules/ytdlp.py
@@ -433,9 +433,9 @@ async def _ytdl(client, message, isLeech=False, sameDir=None, bulk=[]):
             return
         elif up not in ['rcl', 'gd']:
             if up.startswith('mrcc:'):
-                config_path = f'rclone/{message.from_user.id}.conf'
+                config_path = f'zcl/{message.from_user.id}.conf'
             else:
-                config_path = 'rclone.conf'
+                config_path = 'rcl.conf'
             if not await aiopath.exists(config_path):
                 await sendMessage(message, f'Rclone Config: {config_path} not Exists!')
                 return
@@ -464,6 +464,9 @@ async def _ytdl(client, message, isLeech=False, sameDir=None, bulk=[]):
         yt_opt = opt.split('|')
         for ytopt in yt_opt:
             key, value = map(str.strip, ytopt.split(':', 1))
+            if key == 'format' and value.startswith('ba/b-'):
+                qual = value
+                continue
             if value.startswith('^'):
                 if '.' in value or value == '^inf':
                     value = float(value.split('^')[1])
@@ -492,10 +495,8 @@ async def _ytdl(client, message, isLeech=False, sameDir=None, bulk=[]):
     user_id = message.from_user.id
     if not select:
         user_dict = user_data.get(user_id, {})
-        if 'format' in options:
+        if not qual and 'format' in options:
             qual = options['format']
-        elif user_dict.get('yt_opt'):
-            qual = user_dict['yt_opt']
 
     if not qual:
         qual = await YtSelection(client, message).get_quality(result)

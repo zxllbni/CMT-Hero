d4327979d869cc3118dbeeb800849420b32cf364
Pea Masamba <96761006+NadifMasamba@users.noreply.github.com>
2023-12-01 10:47:17 +0700

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -85,7 +85,7 @@ class TelegraphHelper:
                     nxt_page += 1
             await self.edit_page(
                 path=path[prev_page],
-                title='Z Torrent Search',
+                title='Pea Masamba Torrent Search',
                 content=content
             )
         return
